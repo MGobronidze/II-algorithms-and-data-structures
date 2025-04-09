7f09c6e256f84e8324744d5fa79bb039553b0ebf
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2025-04-09 21:41:03 +0400

    add 5th

--- 5/minstack.py
+++ 5/minstack.py
@@ -17,3 +17,26 @@ class MinStack:
     def get_min(self) -> int:
         return self.min_stack[-1]
     
+
+# MinStack კლასის ტესტირება
+min_stack = MinStack()
+
+# ელემენტების დამატება
+min_stack.push(3)
+min_stack.push(5)
+print(min_stack.get_min())  # Output: 3
+
+min_stack.push(2)
+min_stack.push(8)
+print(min_stack.get_min())  # Output: 2
+
+# ბოლო ელემენტის ამოღება
+min_stack.pop()
+print(min_stack.get_min())  # Output: 2
+
+# ბოლო ელემენტის ამოღება და ტოპის ჩვენება
+min_stack.pop()
+print(min_stack.top())  # Output: 2
+
+# მინიმუმის მიღება
+print(min_stack.get_min())  # Output: 3
